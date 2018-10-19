5a4237ec81f3f92f4115ea6ee36120e540127f46
Matt Marcha <spam@aveias.fr>
2018-10-19 17:00:53 +0200

    Remplissage des pages

--- src/pages/buy/buy.ts
+++ src/pages/buy/buy.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the BuyPage page.
@@ -15,11 +16,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BuyPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BuyPage');
+  }
+
+  sendTransaction($amount) {
+    //TODO : Contacter l'API et proposer la transaction. L'API répondra true ou False
+    if (true) {
+      this.navCtrl.popToRoot();
+    }
+    else {
+      const alert = this.alertCtrl.create({
+        title: 'Erreur',
+        subTitle: 'La transaction a échoué. Vérifiez les données saisies et réessayez.',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
   }
 
 }
