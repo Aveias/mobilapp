10cae386cf5846c5c2da5ef7988f16276e4ac944
Matt Marcha <spam@aveias.fr>
2018-10-19 16:21:52 +0200

    Ébauche des modèles user et transaction

---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+import { Transaction } from "./Transaction";
+
+export class User {
+  name: string
+  firstName: string
+  email: string
+  balance: number
+  lastTransactions: Array<Transaction>
+
+}
