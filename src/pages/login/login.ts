d9dea089e41243edf41b26f1af3f66d91f86d8c8
Matt Marcha <spam@aveias.fr>
2018-10-19 14:48:02 +0200

    Formulaire de login et redirection vers page accueil

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the LoginPage page.
@@ -16,15 +17,25 @@ import { HomePage } from '../home/home';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
-  changePage() {
-    this.navCtrl.push(HomePage);
+  validLogin($username, $password) {
+    if ($username === "test" && $password === "test") {
+      this.navCtrl.setRoot(HomePage);
+    }
+    else {
+      const alert = this.alertCtrl.create({
+        title: 'Erreur',
+        subTitle: 'Identifiant ou mot de passe erroné. Veuillez réessayer.',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
   }
 
 }
